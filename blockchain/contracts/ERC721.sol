a101d57207a9d8ddaf9de81b814f908905cb93ac
Mobin <mobinhjzdh@gmail.com>
2022-05-13 16:16:13 +0430

    feat(blockchain): add NFT Royalty Standard

--- blockchain/contracts/ERC721.sol
+++ blockchain/contracts/ERC721.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 
-contract ERC721Marketplace is ERC721 {
+contract ERC721Marketplace is ERC721Royalty {
     uint256 private tokenIdCounter = 1818;
     mapping(uint256 => string) private tokenURIs;
 
@@ -11,10 +11,14 @@ contract ERC721Marketplace is ERC721 {
         ERC721(_name, _symbol)
     {}
 
-    function mint(string memory _tokenURI) external {
+    function mint(string memory _tokenURI, uint96 _royalties)
+        external
+        returns (uint96)
+    {
         uint256 _tokenId = ++tokenIdCounter;
         _safeMint(msg.sender, _tokenId);
         tokenURIs[_tokenId] = _tokenURI;
+        _setTokenRoyalty(_tokenId, msg.sender, _royalties * 100);
     }
 
     function tokenURI(uint256 _tokenId)
