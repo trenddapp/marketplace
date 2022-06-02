d4a6872bb0ea8ab66be9f001e4c0836f30f85db4
Mobin <mobinhjzdh@gmail.com>
2022-06-02 19:07:57 +0430

    feat(blockchain): add ERC721Factory contract

---
+++ blockchain/contracts/NFT721Factory.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./NFT721.sol";
+
+contract NFT721Factory {
+    event ColletionCreated(string name, string symbol, address contractAddress);
+
+    function createCollection(string memory _name, string memory _symbol)
+        external
+    {
+        NFT721 collection = new NFT721(_name, _symbol);
+
+        emit ColletionCreated(_name, _symbol, address(collection));
+    }
+}
