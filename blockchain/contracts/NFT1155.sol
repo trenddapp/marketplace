081f698b5146878484bde08053b51b2fd7d74648
Mobin <mobinhjzdh@gmail.com>
2022-06-18 14:31:49 +0430

    feat(blockchain): add marketplace erc1155 contract

---
+++ blockchain/contracts/NFT1155.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract NFT1155 is ERC2981, ERC1155 {
+    uint256 private tokenIdCounter = 1818;
+    mapping(uint256 => string) private tokenURIs;
+
+    string public constant name = "TrendDapp";
+    string public constant symbol = "TD";
+
+    constructor() ERC1155("") {}
+
+    function uri(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        return tokenURIs[_tokenId];
+    }
+
+    /// @param _royalties Any number * 100
+    function mint(
+        string memory _tokenURI,
+        uint256 amount,
+        uint96 _royalties,
+        bytes memory data
+    ) public {
+        uint256 _tokenId = ++tokenIdCounter;
+        _mint(msg.sender, _tokenId, amount, data);
+        tokenURIs[_tokenId] = _tokenURI;
+        _setTokenRoyalty(_tokenId, msg.sender, _royalties);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC2981, ERC1155)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
