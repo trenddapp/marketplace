5c150c01ee7ac14679a47dc76169025c995677ff
Mobin <mobinhjzdh@gmail.com>
2022-06-19 16:43:40 +0430

    fix(blockchain): fix event name

--- blockchain/contracts/NFT1155Factory.sol
+++ blockchain/contracts/NFT1155Factory.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.8.0;
 import "./NFT1155.sol";
 
 contract NFT1155Factory {
-    event ColletionCreated(string name, string symbol, address contractAddress);
+    event CollectionCreated(
+        string name,
+        string symbol,
+        address contractAddress
+    );
 
     function createCollection(string memory _name, string memory _symbol)
         external
     {
         NFT1155 collection = new NFT1155(_name, _symbol);
 
-        emit ColletionCreated(_name, _symbol, address(collection));
+        emit CollectionCreated(_name, _symbol, address(collection));
     }
 }
