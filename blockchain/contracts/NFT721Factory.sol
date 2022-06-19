da6f1f88c8ad7cae230186b290e851c3899ee994
Mobin <mobinhjzdh@gmail.com>
2022-06-19 16:43:40 +0430

    fix(blockchain): fix event name

--- blockchain/contracts/NFT721Factory.sol
+++ blockchain/contracts/NFT721Factory.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.8.0;
 import "./NFT721.sol";
 
 contract NFT721Factory {
-    event ColletionCreated(string name, string symbol, address contractAddress);
+    event CollectionCreated(
+        string name,
+        string symbol,
+        address contractAddress
+    );
 
     function createCollection(string memory _name, string memory _symbol)
         external
     {
         NFT721 collection = new NFT721(_name, _symbol);
 
-        emit ColletionCreated(_name, _symbol, address(collection));
+        emit CollectionCreated(_name, _symbol, address(collection));
     }
 }
